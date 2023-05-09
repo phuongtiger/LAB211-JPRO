32b5af3a277f92df4a2ba48e4a9031a5902b1c26
Wick <ducsieuda@gmail.com>
2023-05-09 19:00:42 +0700

    sieu update j1.S.P0002

--- J1.S.P0002/src/j1/s/p0002/J1SP0002.java
+++ J1.S.P0002/src/j1/s/p0002/J1SP0002.java
@@ -17,12 +17,22 @@ public class J1SP0002 {
 	 * @param args the command line arguments
 	 */
 	public static void main(String[] args) {
+		 // tạo lớp scanner sc để nhập số phần tử trong mảng
 		Scanner sc = new Scanner(System.in);
-		SelectionSort arr = new SelectionSort();//Create a object point to address has value
 		System.out.println("Enter number of array: ");
-		int number = sc.nextInt();
-		System.out.println("Unsorted array: ");
-		System.out.println("Sorted array: ");
+		int n = sc.nextInt();
+                //tạo mảng arr với số phần tử là n
+		int[] arr = new int[n];
+                // cho random n số phần tử trong mảng
+		for(int i = 0; i < n; i++){
+			int code = (int) Math.floor(((Math.random() * 99)));
+			arr[i] = code;
+		}
+                // tạo đối tượng arr1 để truy xuất dữ liệu
+		SelectionSort arr1 = new SelectionSort(arr);//Create a object point to address has value
+		System.out.println("Unsorted array: " + arr1.toString());
+		arr1.sort();
+		System.out.println("Sorted array: " + arr1.toString());
 	}
 	
 }
--- J1.S.P0002/src/j1/s/p0002/SelectionSort.java
+++ J1.S.P0002/src/j1/s/p0002/SelectionSort.java
@@ -10,5 +10,66 @@ package j1.s.p0002;
  * @author hoang
  */
 public class SelectionSort {
-	
+
+    // tao đối tượng array
+    private int array[];
+
+    /**
+     * tao constructor rỗng
+     */
+    public SelectionSort() {
+    }
+
+    /**
+     *
+     * @param tạo constructor có tham số
+     */
+    public SelectionSort(int[] arr) {
+        this.array = arr;
+    }
+    // hàm setter 
+
+    public void setArr(int[] arr) {
+        this.array = arr;
+    }
+
+    /**
+     *
+     * @hàm getter
+     */
+    public int[] getArr() {
+        return array;
+    }
+
+    /**
+     * hàm sắp xếp mảng bằng phương pháp bubble sort
+     */
+    public void sort() {
+        int temp;
+        int loc;
+        int loc2;
+        int n = array.length;
+        for (int i = 0; i < n; i++) {
+            loc = i;
+            for (int j = i + 1; j < n; j++) {
+                if (array[loc] > array[j]) {
+                    loc = j;
+                }
+            }
+            temp = array[i];
+            array[i] = array[loc];
+            array[loc] = temp;
+
+        }
+    }
+
+    // hàm to string
+    @Override
+    public String toString() {
+        String res = "";
+        for (int i : array) {
+            res += i + ", ";
+        }
+        return "[" + res.substring(0, res.length() - 2) + "]";
+    }
 }
